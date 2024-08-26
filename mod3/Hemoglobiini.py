e0793768af9d2f79a4fa609418883f7d3429428c
Mikae <mikael.mikkola@metropolia.fi>
2024-08-26 17:20:10 +0300

    Kansioa

--- mod3/Hemoglobiini.py
+++ mod3/Hemoglobiini.py
@@ -1,6 +1,18 @@
 sukupuoli = input('Anna biologinen sukupuoli: ')
-hemoglobiiniarvo = input('Anna hemoglobiiniarvo (g/l): ')
-
-sukupuoli = 'mies' or 'nainen'
-hemoglobiiniarvoN = 117 >= 175
-hemoglobiiniarvoM = 134 >= 195
+hemoglobiiniarvo = int(input('Anna hemoglobiiniarvo (g/l): '))
+if sukupuoli == 'mies':
+    if hemoglobiiniarvo < 134:
+        print('Hemoglobiiniarvo alhainen')
+    elif 134 <= hemoglobiiniarvo <= 195:
+        print('Hemoglobiini arvo normaali')
+    elif hemoglobiiniarvo > 195:
+        print('Hemoglobiini arvo korkea')
+elif sukupuoli == 'nainen':
+    if hemoglobiiniarvo < 117:
+        print('Hemoglobiiniarvo alhainen')
+    elif 117 <= hemoglobiiniarvo <= 175:
+        print('Hemoglobiini arvo normaali')
+    elif hemoglobiiniarvo > 175:
+        print('Hemoglobiini arvo korkea')
+else:
+    print('Virheellinen sukupuoli')
