d225fca03cefc51de388b6cf48f9da0ebd00df15
Mikaemik <mikael.e.mikkola@gmail.com>
2024-08-29 14:35:33 +0300

    mod4

--- mod4/teht6.py
+++ mod4/teht6.py
@@ -1,9 +1,12 @@
 import random
 
 N = 100
+n = 0
 iterator = 0
 while iterator < N:
     iterator += 1
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     print(f'Arvottu piste: {x}, {y}')
+    if x**2 + y**2 < 1:
+        print('Piste on yksikköympyrässä.')
