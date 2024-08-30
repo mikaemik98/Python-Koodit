029a9476edb1b9456cabdf4598e3920016dd8c85
Mikae <mikael.mikkola@metropolia.fi>
2024-08-30 09:33:21 +0300

    mod4

--- mod4/teht6.py
+++ mod4/teht6.py
@@ -1,15 +1,17 @@
 import random
 
-#TODO: kysy N arvo käyttäjältä
 N = 100
 n = 0
 iterator = 0
 while iterator < N:
+    N = int(input('Anna arvottavien pisteiden määrä: '))
     iterator += 1
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     print(f'Arvottu piste: {x}, {y}')
     if x**2 + y**2 < 1:
+        n += 1
+        iterator += 1
         print('Piste on yksikköympyrässä.')
-        #TODO: lisää n arvoon 1
-#TODO: tulosta kaavan mukaan laskettu piin likiarvo
+    pi_likiarvo = 4 * n/N
+    print(f'Piin likiarvo on: {pi_likiarvo}')
