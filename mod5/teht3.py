1a931adf86d3e22ea17ab9e22575e38ebbf1dcd5
Mikaemik <mikael.e.mikkola@gmail.com>
2024-09-02 11:46:24 +0300

    mod5

---
+++ mod5/teht3.py
@@ -0,0 +1,8 @@
+
+luku = 0
+anna_luku = int(input('Anna luku: '))
+for luku in range(anna_luku):
+    luku % 1 == 0
+    print(f'Luku {luku} on alkuluku')
+else:
+    print(f'Luku {luku} ei ole alkuluku')
---
+++ tunnit/for rakenne.py
@@ -0,0 +1,3 @@
+numero_lista = [1000, 2, 4, 45, 5, 600, 7, 8 ,9, 10]
+numero_lista.sort(reverse=True)
+print(numero_lista)
