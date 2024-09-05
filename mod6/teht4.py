a9ec017ad8bf8b18745361269ff9a9bac9d4bd34
Mikaemik <mikael.e.mikkola@gmail.com>
2024-09-05 14:05:55 +0300

    mod6

---
+++ mod6/teht4.py
@@ -0,0 +1,14 @@
+import math
+
+def kokonaisluvut(lukuja):
+    summa = 0
+    for luku in lukuja:
+        summa += luku
+    return summa
+
+lista = []
+n = int(input('Määritä monta lukua haluat antaa, jotka lasketaan yhteen: '))
+for i in range(n):
+    lista.append(int(input('Anna luku: ')))
+tulos = kokonaisluvut(lista)
+print(tulos)
