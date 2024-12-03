5f93a2b3c9e117066e9492a54eed885ef9411597
Mikaemik <mikael.e.mikkola@gmail.com>
2024-12-03 14:23:18 +0200

    js

--- peli sivut/peli_kaksiv2/js2.js
+++ peli sivut/peli_kaksiv2/js2.js
@@ -1,10 +1,20 @@
 'use strict';
 
-const map = L.map('map1').setView([60.23, 24.74], 5);
-map.invalidateSize();
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+document.addEventListener('DOMContentLoaded', function() {
+  const map = L.map('map1').setView([60.23, 24.74], 5);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  setTimeout(() => {
+    map.invalidateSize();
+  }, 100);
+
+  window.addEventListener('resize', () => {
+    map.invalidateSize();
+  });
+});
 
 function toggleFullScreen() {
       if (!document.fullscreenElement) {
